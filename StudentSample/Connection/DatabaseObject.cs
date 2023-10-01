7ca642186c4998221dc984d32ef7d20ea2340454
kyawe225 <kyawe225@gmail.com>
2023-10-01 07:30:46 +0630

    database object update for no warning

--- StudentSample/Connection/DatabaseObject.cs
+++ StudentSample/Connection/DatabaseObject.cs
@@ -3,11 +3,11 @@ namespace StudentSample.Connection
 {
     public class DatabaseObject
     {
-        public string Host;
+        public string Host=string.Empty;
         public int Port;
-        public string Database;
-        public string Username;
-        public string Password;
+        public string Database=string.Empty;
+        public string Username=string.Empty;
+        public string Password=string.Empty;
 
         public DatabaseObject()
         {
